6f8ffc1bd91a2443fbe98dbda60ac9cde2da9f32
Raulcasallas26 <rcasallas26@gmail.com>
2023-08-18 16:43:06 -0500

    server ok

--- buses_bakend/models/server.js
+++ buses_bakend/models/server.js
@@ -31,6 +31,7 @@ class Server {
     middlewares() {
         this.app.use(express.json())
         this.app.use(cors())
+        this.app.use(express.static('public'))
     }
     escuchar() {
         this.app.listen(process.env.PORT, () => {
--- buses_frontend/src/routes/direccion.js
+++ buses_frontend/src/routes/direccion.js
@@ -1,2 +1,3 @@
 const urlBackend="http://localhost:4201"
 export{urlBackend}
+// http://localhost:4201
